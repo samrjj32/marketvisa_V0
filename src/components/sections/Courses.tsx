a200de523ea6fd9cfb09a3df6f51fc368801aaf9
samrajj32 <samrajj3223@gmail.com>
2025-03-09 02:36:20 +0000

    fix sm

--- src/components/sections/Courses.tsx
+++ src/components/sections/Courses.tsx
@@ -217,18 +217,18 @@ export default function Courses() {
               ))}
             </div>
           </div>
-          <div  className="flex justify-center mb-15">
-          <Button 
-            variant="primary"
-            size="lg"
-            onClick={() => handlePayment()}
-            className="w-full md:w-auto px-4 md:px-12 py-2 md:py-4 text-sm md:text-lg font-bold shadow-xl hover:shadow-2xl hover:scale-105 transition-all flex items-center justify-center gap-2 bg-[#00D066]"
-          >
-            <span className="text-center">
-              Book your slot and get one of our premium paid features for free.
-            </span>
-            <FaArrowRight className="w-3 h-3 md:w-4 md:h-4 flex-shrink-0" />
-          </Button>
+          <div className="flex justify-center mb-16">
+            <Button 
+              variant="primary"
+              size="lg"
+              onClick={handlePayment}
+              className="w-full md:w-auto px-4 md:px-12 py-2 md:py-4 text-sm md:text-lg font-bold shadow-xl hover:shadow-2xl hover:scale-105 transition-all flex items-center justify-center gap-2 bg-[#00D066]"
+            >
+              <span className="text-center">
+                Book your slot and get one of our premium paid features for free.
+              </span>
+              <FaArrowRight className="w-3 h-3 md:w-4 md:h-4 flex-shrink-0" />
+            </Button>
           </div>
          
         </div>
