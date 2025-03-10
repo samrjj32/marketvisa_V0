3277e9caede29f988bdff5c604d19a9eba851336
samrajj32 <samrajj3223@gmail.com>
2025-03-10 10:37:16 +0000

    vercel, facebook analytics fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,17 @@ import "./globals.css";
 import Header from "@/components/layout/Header";
 import { Toaster } from 'react-hot-toast';
 import MarqueeBanner from "@/components/common/MarqueeBanner";
-
+import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "MarketVisa",
   description: "Transform your financial future with expert-led courses",
+  icons: {
+     icon: '/favicon.ico',
+    shortcut: '/favicon.ico'
+  },
 };
 
 export default function RootLayout({
@@ -20,11 +25,34 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* <link rel="icon" href="/favicon.ico" sizes="any" /> */}
+        {/* Optionally add different sizes/formats */}
+        <link rel="icon" href="/mk_visa_icon.ico" />
+        {/* Move Meta Pixel Code inside Script component */}
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '8684248018341312');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
-      <MarqueeBanner />
+        <MarqueeBanner />
         <main className="min-h-screen pt-1">
           {children}
+          <Analytics />
         </main>
        
         {/* <Footer /> */}
